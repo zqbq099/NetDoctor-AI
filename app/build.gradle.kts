bb829f9e1f6de796a3ec69897ca8459305d188b3
zqbq099 <zqbq099@gmail.com>
2026-04-15 23:35:42 +0300

    Update minSdk version from 21 to 26

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.netdoctor"
-        minSdk = 21
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"

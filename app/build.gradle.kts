325bb32e19353ab372b5dffd396b5fcf9912bf66
zqbq099 <zqbq099@gmail.com>
2026-04-16 17:51:07 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,7 +52,7 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
     
     implementation("com.google.android.gms:play-services-ads:22.6.0")
+    
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("com.google.code.gson:gson:2.10.1")
 }
-
-implementation("com.squareup.okhttp3:okhttp:4.12.0")
-implementation("com.google.code.gson:gson:2.10.1")
